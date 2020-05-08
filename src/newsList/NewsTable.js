f9f598ce8c7c32c640efe0da144bc20d7a90ca95
Munoon <Munoongg@gmail.com>
2020-05-08 13:00:46 +0300

    Change news date formate

--- src/newsList/NewsTable.js
+++ src/newsList/NewsTable.js
@@ -44,5 +44,8 @@ class NewsCard extends React.Component {
 }
 
 function formatDate(date) {
-    return `${date.getDay()}.${date.getMonth()}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+    const dtf = new Intl.DateTimeFormat('en', 
+        { year: 'numeric', month: '2-digit', day: '2-digit', minute: '2-digit', hour: '2-digit' });
+    const [{ value: month },, { value: day },, { value: year },, { value: hour },, { value: minute }] = dtf.formatToParts(date);
+    return `${day}.${month}.${year} ${hour}:${minute}`;
 }
