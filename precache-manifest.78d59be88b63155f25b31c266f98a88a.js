100b0035ce688990382278cde83637e16f58d873
Munoon <Munoongg@gmail.com>
2020-06-08 22:24:38 +0300

    Add data

---
+++ precache-manifest.78d59be88b63155f25b31c266f98a88a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df8985cb29b9044da09ac1db17a15b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c413d79e974e7c72184d",
+    "url": "/static/css/2.59405167.chunk.css"
+  },
+  {
+    "revision": "c413d79e974e7c72184d",
+    "url": "/static/js/2.b4bed628.chunk.js"
+  },
+  {
+    "revision": "2509af0d75eb64f4d8445baa31f1ad06",
+    "url": "/static/js/2.b4bed628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf667ac2067c47461468",
+    "url": "/static/js/main.551895c2.chunk.js"
+  },
+  {
+    "revision": "1d8cbb29e84cbbd88996",
+    "url": "/static/js/runtime-main.9a35d1b2.js"
+  }
+]);
--- src/newsList/NewsList.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { NewsTable } from './NewsTable';
-
-export class NewsList extends React.Component {
-    render() {
-        return <NewsTable news={this.props.data} />;
-    }
-}
